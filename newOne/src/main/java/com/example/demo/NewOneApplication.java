f7cb08f730f4c5b4db5a7e730777da9a6cf1fc10
Piyush  Shinde <119596568+Piyushshind@users.noreply.github.com>
2024-12-20 12:35:56 +0530

    Add files via upload
    controller

---
+++ newOne/src/main/java/com/example/demo/NewOneApplication.java
@@ -0,0 +1,14 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NewOneApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NewOneApplication.class, args);
+		System.out.println("hello world from piyush2..............");
+	}
+
+}
