8155387eaa69d7875680127e9afc218a31e53ead
Piyush Shinde <piyushshinde.199@gmail.com>
2024-12-27 13:16:04 +0530

    Added tymeleafJpaproject to the repository

---
+++ Projects/tymeleafJpaproject/src/main/java/com/example/tymeleafJpaproject/TymeleafApplication.java
@@ -0,0 +1,16 @@
+package com.example.tymeleafJpaproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TymeleafApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TymeleafApplication.class, args);
+
+        System.out.println("application started ................");
+
+    }
+
+}
